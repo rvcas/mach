fb55aca32eb2422ae12c8799c5dac006467bb0d1
rvcas <x@rvcas.dev>
2024-09-14 19:27:22 -0400

    chore: update tauri

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri";
+  import { invoke } from "@tauri-apps/api/core";
   import { addTodo, db, type Todo } from "$lib/instant";
 
   let name = $state("");
